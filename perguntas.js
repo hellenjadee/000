f51429db0466027d58e3e16dc9eaffcc45bcb30b
hellenjadee <148216696+hellenjadee@users.noreply.github.com>
2024-09-25 23:15:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'ciencia',
+    'Qual é a principal função da clorofila nas plantas??',
+    'A clorofila é responsável pela fotossíntese, permitindo que as plantas convertem luz solar em energia.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior rio do mundo em volume de água?',
+    'o rio amazonas'
+)
+
+criaCartao(
+    'Historia',
+    'Quem foi o líder da Revolução Francesa que se tornou imperador da França??',
+    'Napoleão Bonaparte.  ' 
+
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'O que é uma metáfora?  ?',
+    'Uma metáfora é uma figura de linguagem que estabelece uma comparação implícita entre dois elementos, sem usar conectivos como "como".'
+)
